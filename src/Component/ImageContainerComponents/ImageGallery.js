41636e9eef9367d23651365daa47beb1905fee77
joeyrobinson <joeyrobinsondev@gmail.com>
2018-06-11 15:24:17 -0500

    Stuff

--- src/Component/ImageContainerComponents/ImageGallery.js
+++ src/Component/ImageContainerComponents/ImageGallery.js
@@ -1,52 +1,11 @@
 import React from 'react';
 
-const imageOne = `http://via.placeholder.com/1000x667`;
-
 const ImageGallery = () => (
   <section className="ImageGallery">
-    From Image Gallery
-    {/* <figure className="GalleryItem GalleryItem-1">
-      <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-2">
-      <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-3">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-4">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-5">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-6">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-7">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-8">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-9">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-10">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-11">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-12">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-13">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure>
-    <figure className="GalleryItem GalleryItem-14">
-    <img src={imageOne} alt="" className="GalleryImage" />
-    </figure> */}
+    <img className="ImageGallery-Image" src="https://s3-us-west-2.amazonaws.com/sscstaticsitejr/ssc001.jpg" alt=""/>
+    <img className="ImageGallery-Image" src="https://s3-us-west-2.amazonaws.com/sscstaticsitejr/ssc001.jpg" alt=""/>
+    {/* <img className="ImageGallery-Image" src="https://s3-us-west-2.amazonaws.com/sscstaticsitejr/ssc001.jpg" alt=""/> */}
+    {/* <img className="ImageGallery-Image" src="https://s3-us-west-2.amazonaws.com/sscstaticsitejr/ssc001.jpg" alt=""/> */}
   </section>
 
 );
