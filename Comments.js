b5927d0b02be886a3715dafe764d59d0e936d203
elamahpla <theamazingness@gmail.com>
2018-06-14 21:50:52 -0500

    My. Updating Comments.js

--- Comments.js
+++ Comments.js
@@ -2,3 +2,12 @@
 
 // My package.json file is pretty wacky right now. I'm going to have to update it to see if I can remove the "one:won" script
 // And just rely on npm start. Forcing someone to use both scripts is unintuitive and unreasonable
+
+// I've given some thought into using reach-router (https://github.com/reach/router), but I want to study that a bit more before
+// Making that plunge. One thing I'm going to try here, which I haven't done in a live site is minimal media queries.
+// I oftentimes find myself overusing media queries, but I think that's due to a lack of CSS capabilities. This next project
+// (As well as every project afterwards) will make use of CSS Grid. Flex is amazing, but Grid is so fluid. 
+
+// As well as using minimal media queries, I'm going to aim for minimal usage of external libraries as well (eg: materialize)
+// I personally feel I've been using them too much when I can learn more by simply making the item myself. Example: making a modal
+// I was relying too heavily on react-modal instead of just making one myself. 
